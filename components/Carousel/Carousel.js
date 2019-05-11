d0e5a27f5605eae37f2f85844b8906202c2a3fd1
LadyKerr <kadiekerr@gmail.com>
2019-05-11 14:04:41 -0400

    started attempting stretch

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,13 @@
 class Carousel {
-
+    constructor(slide) {
+    this.slide = slide;
+    this.left = this.left;
+    this.right = this.right;
+}
 }
 
-let carousel = document.querySelector();
+let carousel = document.querySelector('.carousel');
+console.log(carousel);
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
